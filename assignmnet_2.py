10425763d2fc3073529854a2fd8c59b6644bdc43
rithvikdemon <40763621+rithvikdemon@users.noreply.github.com>
2018-07-03 12:20:41 +0530

    Add files via upload

---
+++ assignmnet_2.py
@@ -0,0 +1,25 @@
+print ("AcadView")
+
+a= "Acad"
+b= "View"
+print(a+b)
+
+
+x= 1
+y= 2
+z= 3
+print (x,y,z)
+
+print("Lets get started")
+
+
+s= "AcadView"
+course= "Python"
+fees= 5000
+
+print('%s %s %d'%(s,course,fees))
+
+
+name="Tony Stark"
+salary= 1000000
+print('%s %d' %(name,salary))
