00b2edbc0dc65cb001e3a235b92794cf8fdc493e
rithvikdemon <40763621+rithvikdemon@users.noreply.github.com>
2018-07-03 13:04:03 +0530

    Add files via upload

---
+++ assignmnet_3.py
@@ -0,0 +1,41 @@
+#Question1
+
+a= [input ("Enter the values") for i in range(5)]
+print (a)
+
+#Question2
+
+b= ['google','apple','facebook','microsoft','tesla']
+c=a+b
+print (c)
+
+#Question3
+
+print(c.count('a'))
+
+#Question4
+
+d= [1,2,3,9,8,7]
+print(d)
+d.sort()
+print (d)
+
+#Question5
+
+A= [9,8,7]
+B= [1,2,3]
+C=A+B
+
+C.sort()
+print(C)
+
+#Question6
+
+stack = ["Chandan", "Shilpa", "Akshay"]
+stack.append("Rithvik")
+stack.append("Shivam")
+print(stack)
+print(stack.pop())
+print(stack)
+print(stack.pop())
+print(stack)
