8ce5e4d1e4be91ab3175f1f45b77b8de8d5baf61
unknown <rithvik.kumar96@gmail.com>
2018-07-03 11:59:57 +0530

    hello.py

---
+++ hello.py
@@ -0,0 +1,9 @@
+Python 3.7.0 (v3.7.0:1bf9cc5093, Jun 27 2018, 04:06:47) [MSC v.1914 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> print ("hello")
+hello
+>>> 
+================= RESTART: C:/Users/RITHVIK/Desktop/hello.py =================
+hello1
+>>> 
+================= RESTART: C:/Users/RITHVIK/Desktop/hello.py =================
